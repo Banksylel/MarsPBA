12f0720c971f1dce499593747b3caaf31ba1e9f0
chris endacott <chris.endacott@live.co.uk>
2020-11-12 14:27:00 +0000

    Don't ask

--- functions/mars/data_pre_processing_pipeline.R
+++ functions/mars/data_pre_processing_pipeline.R
@@ -4,12 +4,15 @@
 #   DATA PRE-PROCESSING PIPELINE
 #
 #   
-#   DATE:     11 NOVEMBER 2020
+#   DATE:     08 NOVEMBER 2020
 #   VERSION:  V1.0
 #   AUTHOR:   MARS Team
 #
 #   UPDATE
-#   1.00      11/11/2020    Chris Endacott    Initial Version
+#   1.00      08/11/2020    Chris Jennings    Initial Version
+#   1.01      08/11/2020    Brian Nyathi      + Balancing of dataset
+#   1.02      09/11/2020    Ryan Banks        Called functions for splitting the dataset with k-fold cross validation
+#   1.03      11/11/2020    Chris Jennings    Source all functions files recursively.
 # *************************************************************
 
 #  clears all objects in "global environment"
@@ -64,10 +67,26 @@ test <- function(train,test,plot=TRUE){
 }
 
 
+######## Main Function ########
 
+# ************************************************
+# main() :
+# Main entry point
+#
+# INPUT       :   None
+#
+# OUTPUT      :   None
+#
+# ************************************************
+mars_GetPreprocessedDataset<-function(printflag){
+  
+  if(printflag){
+    print("Inside main function")
+  }
 
-
-dummyPrepocessing <-  function(dataset, printFlag = FALSE){
+  # ************************************************
+  # Read data from file
+  dataset<-NreadDataset(DATASET_FILENAME)
   
   #*************************************************
   #Return all rows that have churned. There are 1869 rows
@@ -80,57 +99,68 @@ dummyPrepocessing <-  function(dataset, printFlag = FALSE){
   n_final <- n_shuffled[1:1869, ]  
   #Return balanced and shuffled dataset
   yandn <- rbind(y, n_final)
-  telco <- yandn[sample(1:nrow(yandn)), ]
+  dataset <- yandn[sample(1:nrow(yandn)), ]
   
   # ************************************************
   # Remove customerID field because it is irrelevant
-  telco<-subset(telco, select = -customerID)
+  dataset<-subset(dataset, select = -customerID)
   
   # ************************************************
   # Cast SeniorCitizen to string so that it is interpreted as categorical
-  telco$SeniorCitizen<-as.character(telco$SeniorCitizen)
+  dataset$SeniorCitizen<-as.character(dataset$SeniorCitizen)
   
   # ************************************************
   # Set TotalCharges to zero where tenure is zero
-  telco[which(telco$tenure==0),"TotalCharges"]<--0
+  dataset[which(dataset$tenure==0),"TotalCharges"]<--0
   
   # ************************************************  
   # One-hot encoding special cases
-  telco<-PREPROCESSING_one_hot_special(telco)
+  dataset<-PREPROCESSING_one_hot_special(dataset)
   
   
   # ************************************************
   #
-  NPREPROCESSING_prettyDataset(telco)
+  NPREPROCESSING_prettyDataset(dataset)
   
   # ************************************************
   # Determine if the field appears numeric or symbolic
-  field_types<-NPREPROCESSING_initialFieldType(telco)
+  field_types<-NPREPROCESSING_initialFieldType(dataset)
   
   # ************************************************
   # View the field types on the console
   
-  numeric_fields<-names(telco)[field_types=="NUMERIC"]
-  print(paste("NUMERIC FIELDS=",length(numeric_fields)))
-  print(numeric_fields)
+  numeric_fields<-names(dataset)[field_types=="NUMERIC"]
+  if(printflag){
+    print(paste("NUMERIC FIELDS=",length(numeric_fields)))
+    print(numeric_fields)
+  }
+
   
-  symbolic_fields<-names(telco)[field_types=="SYMBOLIC"]
-  print(paste("SYMBOLIC FIELDS=",length(symbolic_fields)))
-  print(symbolic_fields)
+  symbolic_fields<-names(dataset)[field_types=="SYMBOLIC"]
+  
+  if(printflag){
+    print(paste("SYMBOLIC FIELDS=",length(symbolic_fields)))
+    print(symbolic_fields)
+  }
+
   
   # ************************************************
   # Determine if the numeric fields might be discreet numeric
   
-  field_types1<-NPREPROCESSING_discreetNumeric(dataset=telco,
+  field_types1<-NPREPROCESSING_discreetNumeric(dataset=dataset,
                                                field_types=field_types,
                                                cutoff=DISCREET_BINS)
   
-  results<-data.frame(field=names(telco),initial=field_types,types1=field_types1)
-  print(formattable::formattable(results))
+  results<-data.frame(field=names(dataset),initial=field_types,types1=field_types1)
   
+  if(printflag){
+    print(formattable::formattable(results))
+    
+  }
+
   # ************************************************
   # This is a sub-set frame of just the ordinal fields
-  ordinals<-telco[,which(field_types1==TYPE_ORDINAL)]
+  ordinals<-dataset[,which(field_types1==TYPE_ORDINAL)]
   
   # Replace outlying ordinals with mean values
   ordinals<-NPREPROCESSING_outlier(ordinals=ordinals,confidence=OUTLIER_CONF)
@@ -144,98 +174,62 @@ dummyPrepocessing <-  function(dataset, printFlag = FALSE){
   
   # ************************************************
   # Process the categorical (symbolic/discreet) fields using 1-hot-encoding
-  catagoricalReadyforML<-NPREPROCESSING_categorical(dataset=telco,field_types=field_types1)
+  catagoricalReadyforML<-NPREPROCESSING_categorical(dataset=dataset,field_types=field_types1)
   
-  print(formattable::formattable(data.frame(fields=names(catagoricalReadyforML))))
-  
+  if(printflag){
+    print(formattable::formattable(data.frame(fields=names(catagoricalReadyforML))))
+    
+  }
+
   # Number of non-numeric fields before transformation
   # Which fields are either SYMBOLIC or DISCREET
   nonNumericbefore<-length(which(field_types1!=TYPE_ORDINAL))
   
   # How many fields have be generated through the 1-hot-encoding process
   nonNumerictranformed<-ncol(catagoricalReadyforML)
-  print(paste("Symbolic fields. Before encoding=",nonNumericbefore,"After",nonNumerictranformed))
   
-  # Output the names of the encoded fields (literals)
-  print(formattable::formattable(data.frame(field=1:nonNumerictranformed,encoded=names(catagoricalReadyforML))))
-  
+  if(printflag){
+    print(paste("Symbolic fields. Before encoding=",nonNumericbefore,"After",nonNumerictranformed))
+    print(formattable::formattable(data.frame(field=1:nonNumerictranformed,encoded=names(catagoricalReadyforML))))
+    
+    
+  }
+
   # ************************************************
   # Combine the two sets of data that are read for ML
   combinedML<-cbind(ordinalReadyforML,catagoricalReadyforML)
   
   # Remove redundant fields
   combinedML<-NPREPROCESSING_redundantFields(dataset=combinedML,cutoff=OUTLIER_CONF)
+  
+  
+  ### Data Splitting ###
+  
   #assigns a fold ID to each data entry
   newDataset <- PREPROCESSING_stratDataset(combinedML, KFOLDS)
   
   
-  return(newDataset)
-}
-
-
-
-
-
-
-######## Main Function ########
-
-# ************************************************
-# main() :
-# Main entry point
-#
-# INPUT       :   None
-#
-# OUTPUT      :   None
-#
-# ************************************************
-main<-function(){
-  
-  # Read data from file
-  telco <- NreadDataset(DATASET_FILENAME)
-  
-  print(head(telco))
-  
-  telcoPreprocessed <- dummyPrepocessing(telco)
-  print(head(telcoPreprocessed))
-  
-
-  positionOutput<-which(names(telcoPreprocessed)==OUTPUT_FIELD)
-  predictors<-telcoPreprocessed[,-positionOutput]
-  
-  
-  
-  modelKmeans <- kmeans(x=predictors, centers=4, nstart=25)
-  
-
-  print(str(modelKmeans))
-  
-  # ************************************************
-  # cluster plot of the 4 clusters (uses ggplot2 library)
-  
-  p<-factoextra::fviz_cluster(modelKmeans, data = predictors,geom = "point")
-  print(p)
-  
-  results<-list()
-  r<-1
-  for (k in 2:5){
-    modelKmeans <- kmeans(predictors, k, nstart=25)
-    results[[r]]<-factoextra::fviz_cluster(modelKmeans, data = predictors,geom = "point") + ggtitle(paste("k=",k))
-    r<-r+1
+  #this creates test and train data for each k fold.
+  ######## the name of the dataframes is either test or train with the kfold number infront of it##########
+  ########            eg. test2 = test dataset for the 2nd kfold                                ##########                 
+  for (k in 1:KFOLDS){
+    newDataset <- newDataset[order(runif(nrow(newDataset))),]
+    splitData <- PREPROCESSING_stratSplit(newDataset,KFOLDS)
+    trainAssign <- paste0("train",k)
+    testAssign <- paste0("test",k)
+    assign(trainAssign, splitData$train)
+    assign(testAssign, splitData$test)
   }
-  gridExtra:: grid.arrange(grobs=results, nrow = 2)
   
-  p<-factoextra::fviz_nbclust(predictors, kmeans, method = "wss",k.max = 10)
-  print(p)
+  #**************** after the ml model is run, the mean of the results for each kfold should be returned **************
   
-  p<-factoextra::fviz_nbclust(predictors, kmeans, method = "silhouette",k.max = 15)
-  print(p)
-  
-  gap_stat <- cluster::clusGap(predictors, FUN = kmeans, nstart = 25, K.max = 15, B = 50)
-  p<-factoextra::fviz_gap_stat(gap_stat)
-  print(p)
   
   # The dataset for ML information
+  print(paste("Fields=",ncol(newDataset)))
   print("End")
+  
+  
+  return(newDataset)
   
 } #endof main()
 
@@ -260,11 +254,9 @@ cat("\014")
 library(pacman)
 pacman::p_load(char=MYLIBRARIES,install=TRUE,character.only=TRUE)
 
+source("functions/nick/lab3dataPrep.R")
+source("functions/mars/data_pre_processing_functions.R")
 
-#Source functions
-source("lab3dataPrep.R")    # From Prof Nick's lab
-source("lab2functions.R")   # From Prof Nick's lab
-source("data_pre_processing_functions.R")
 
 # Reset the pseudo-random number generator to start at the same point
 set.seed(123)
@@ -273,6 +265,6 @@ print("PBA TEAM MARS: DATA PRE-PROCESSING PIPELINE")
 
 # ************************************************
 # Call the main function
-main()
+mars_GetPreprocessedDataset(TRUE)
 
 print("end")
--- functions/mars/random_forest_functions.R
+++ functions/mars/random_forest_functions.R
@@ -5,11 +5,89 @@
 #   RANDOM FOREST FUNCTIONS
 #
 #   
-#   DATE:     11 NOVEMBER 2020
+#   DATE:     1 NOVEMBER 2020
 #   VERSION:  V1.0
 #   AUTHOR:   MARS Team
 #
 #   UPDATE
 #   1.00      11/11/2020    Chris Jennings    Initial Version
+#   1.10      12/11/2020     
 
-print("Sourcing random_forest_functions.R ")
+#  clears all objects in "global environment"
+rm(list=ls())
+
+# ************************************************
+#GLOBALS
+
+KFOLDS           <- 5
+
+# Define and then load the libraries used in this project
+# Library from CRAN     Version
+# pacman	               0.5.1
+# outliers	             0.14
+# corrplot	             0.84
+# MASS	                 7.3.53
+# formattable 	         0.2.0.1
+# stats                  4.0.3
+# PerformanceAnalytics   2.0.4
+
+MYLIBRARIES<-c("outliers",
+               "corrplot",
+               "MASS",
+               "formattable",
+               "stats",
+               "caret",
+               "stringr",
+               "PerformanceAnalytics")
+
+
+######## Main Function ########
+
+# ************************************************
+# main() :
+# Main entry point
+#
+# INPUT       :   None
+#
+# OUTPUT      :   None
+#
+# ************************************************
+main<-function(){
+  print("Cat")
+  
+  mars_GetPreprocessedDataset()
+
+} #endof main()
+
+# ************************************************
+# This is where R starts execution
+
+# Automatically release memory
+gc()
+
+# Clear plots and other graphics in RStudio output
+if(!is.null(dev.list())) dev.off()
+graphics.off()
+
+# Clear all warning messages
+assign("last.warning", NULL, envir = baseenv())
+
+# Clears the RStudio console area
+cat("\014")
+
+# If library not already on your computer this will download and
+# install the library. Each library is activated.
+library(pacman)
+pacman::p_load(char=MYLIBRARIES,install=TRUE,character.only=TRUE)
+
+
+# Reset the pseudo-random number generator to start at the same point
+set.seed(123)
+
+print("PBA TEAM MARS: Run Forest Run")
+
+# ************************************************
+# Call the main function
+main()
+
+print("end")
